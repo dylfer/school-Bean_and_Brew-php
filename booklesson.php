986eba0fa7b9e48cd5b6b54655fc2ae8b2cc6c24
Dylan Ferrow <ferrow.dylan.code@gmail.com>
2024-10-12 22:00:16 +0100

    wip: booking pages

---
+++ booklesson.php
@@ -0,0 +1,20 @@
+<?php
+require 'components/session.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>book a lesson</title>
+
+    <link href="stlye.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    Hello world
+    <script src="script.js"></script>
+  </body>
+</html>

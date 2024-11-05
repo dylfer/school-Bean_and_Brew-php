66d38107caa2f724271f490d2336c73e8d474e31
Dylan Ferrow <ferrow.dylan.code@gmail.com>
2024-11-05 23:37:37 +0000

    wip: clean up

--- components/book.php
+++ components/book.php
@@ -1,8 +1,6 @@
 <?php
-include 'functions/uuid.php';
-include 'components/DB_conect.php';
 if( $book == "baking"){
-    $stmt_session = $mysqli->prepare("INSERT INTO baking_bookings () VALUES (?)"); $stmt_session->bind_param("s", );
+    $stmt_session = $mysqli->prepare("INSERT INTO baking_bookings ($user_id,$name,$booking_date,$booking_time,$location,) VALUES (?)"); $stmt_session->bind_param("s", );
     $stmt_session->execute();
     $stmt_session->close();
 }else{
